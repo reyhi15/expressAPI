944f7d0d727a76f5bd34507279107644188f31a1
Renzo Pillman <renzo.pillman@sentinelperu.local>
2020-03-02 12:10:20 -0500

    quinto commit

--- config/database.js
+++ config/database.js
@@ -8,7 +8,7 @@ function dbconnect() {
     // config for your database
     var config = {
         user: 'sa',
-        password: 'Sentinel2010$',
+        password: 'Sentinel2010',
         server: 'SNTNL-PC-RGPS',
         database: 'wsdatabase',
         options: {
